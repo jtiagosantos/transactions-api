98daac476e0790d8b925ae3d466b16e0b1559d39
jtiagosantos <tiagosan040@gmail.com>
2023-04-13 22:22:31 -0300

    chore: configure lint

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import fastify from 'fastify';
 
 const app = fastify();
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server Running');
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running');
+  });
