2a50c2cc65fad52784dcf85eed14b4b276b3e5d2
jtiagosantos <tiagosan040@gmail.com>
2023-04-18 22:57:28 -0300

    feat: implement endpoint to return transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,15 +4,43 @@ import { knex } from '../lib/knex';
 import type { FastifyInstance } from 'fastify';
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async (request, reply) => {
+    try {
+      const findOneTransactionRequestParamSchema = z.object({
+        id: z
+          .string({ required_error: 'id is a required request param' })
+          .uuid({ message: 'id should be a uuid' }),
+      });
+
+      const { id } = findOneTransactionRequestParamSchema.parse(request.params);
+
+      const transaction = await knex('transactions').where({ id }).first();
+
+      return { transaction };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return reply
+          .status(400)
+          .send({ error: error.errors.map((error) => error.message) });
+      }
+    }
+  });
+
   app.post('/', async (request, reply) => {
     try {
-      const createTransactionBodySchema = z.object({
+      const createOneTransactionBodySchema = z.object({
         title: z.string({ required_error: 'title is a required field' }),
         amount: z.number({ required_error: 'amount is a required field' }),
         type: z.enum(['credit', 'debit'], { required_error: 'type is a required field' }),
       });
 
-      const { title, amount, type } = createTransactionBodySchema.parse(request.body);
+      const { title, amount, type } = createOneTransactionBodySchema.parse(request.body);
 
       await knex('transactions').insert({
         id: randomUUID(),
