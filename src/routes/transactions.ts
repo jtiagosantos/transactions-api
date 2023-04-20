3b414d13e962bbed938866977f79161015eccf12
jtiagosantos <tiagosan040@gmail.com>
2023-04-19 22:26:58 -0300

    feat: save sessionId on cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,41 +2,73 @@ import { randomUUID } from 'node:crypto';
 import { z, ZodError } from 'zod';
 import { knex } from '../lib/knex';
 import type { FastifyInstance } from 'fastify';
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select();
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
 
-    return { transactions };
-  });
+      const transactions = await knex('transactions')
+        .where('session_id', '=', sessionId)
+        .select();
 
-  app.get('/:id', async (request, reply) => {
-    try {
-      const findOneTransactionRequestParamSchema = z.object({
-        id: z
-          .string({ required_error: 'id is a required request param' })
-          .uuid({ message: 'id should be a uuid' }),
-      });
+      return { transactions };
+    },
+  );
 
-      const { id } = findOneTransactionRequestParamSchema.parse(request.params);
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      try {
+        const { sessionId } = request.cookies;
 
-      const transaction = await knex('transactions').where({ id }).first();
+        const findOneTransactionRequestParamSchema = z.object({
+          id: z
+            .string({ required_error: 'id is a required request param' })
+            .uuid({ message: 'id should be a uuid' }),
+        });
 
-      return { transaction };
-    } catch (error) {
-      if (error instanceof ZodError) {
-        return reply
-          .status(400)
-          .send({ error: error.errors.map((error) => error.message) });
+        const { id } = findOneTransactionRequestParamSchema.parse(request.params);
+
+        const transaction = await knex('transactions')
+          .where({ id, session_id: sessionId })
+          .first();
+
+        return { transaction };
+      } catch (error) {
+        if (error instanceof ZodError) {
+          return reply
+            .status(400)
+            .send({ error: error.errors.map((error) => error.message) });
+        }
       }
-    }
-  });
+    },
+  );
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions').sum('amount', { as: 'amount' }).first();
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
 
-    return { summary };
-  });
+      const summary = await knex('transactions')
+        .where({ session_id: sessionId })
+        .sum('amount', { as: 'amount' })
+        .first();
+
+      return { summary };
+    },
+  );
 
   app.post('/', async (request, reply) => {
     try {
@@ -48,10 +80,22 @@ export const transactionsRoutes = async (app: FastifyInstance) => {
 
       const { title, amount, type } = createOneTransactionBodySchema.parse(request.body);
 
+      let sessionId = request.cookies.sessionId;
+
+      if (!sessionId) {
+        sessionId = randomUUID();
+
+        reply.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
+        });
+      }
+
       await knex('transactions').insert({
         id: randomUUID(),
         title,
         amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId,
       });
 
       return reply.status(201).send();
