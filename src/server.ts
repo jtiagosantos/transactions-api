7185f2ce226f884340ec090b2fe9f0a4f46eafbb
jtiagosantos <tiagosan040@gmail.com>
2023-04-20 00:06:00 -0300

    refactor: separate app of server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,5 @@
-import fastify from 'fastify';
+import { app } from './app';
 import { env } from './env';
-import cookie from '@fastify/cookie';
-
-import { transactionsRoutes } from './routes/transactions';
-import { logs } from './logs';
-
-const app = fastify();
-
-app.addHook('preHandler', logs);
-
-app.register(cookie);
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-});
 
 app
   .listen({
