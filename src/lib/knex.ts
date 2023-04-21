5c1b088da08d6bafa0b7c1b1ab0074be723023dd
jtiagosantos <tiagosan040@gmail.com>
2023-04-20 23:56:41 -0300

    chore: prepare app to deploy

--- src/lib/knex.ts
+++ src/lib/knex.ts
@@ -2,10 +2,13 @@ import { knex as setupKnex, Knex } from 'knex';
 import { env } from '../env';
 
 export const config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.BATABASE_CLIENT,
+  connection:
+    env.BATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
