d0038918303fe7ff2e6260a7869397933dc8db34
jtiagosantos <tiagosan040@gmail.com>
2023-04-25 23:56:33 -0300

    fix: remove unucessary done

--- src/middlewares/check-session-id-exists.ts
+++ src/middlewares/check-session-id-exists.ts
@@ -1,15 +1,12 @@
-import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 
 export const checkSessionIdExists = async (
   request: FastifyRequest,
   reply: FastifyReply,
-  done: HookHandlerDoneFunction,
 ) => {
   const sessionId = request.cookies.sessionId;
 
   if (!sessionId) {
     return reply.status(401).send({ error: 'Unauthorized' });
   }
-
-  done();
 };
